44666970f49fb5896c896b49704423f97935e3d6
Ann <ania.somko@gmail.com>
2021-12-31 01:43:20 +0200

    change buttons and methods

--- canvas.js
+++ canvas.js
@@ -11,21 +11,6 @@ const scaleY = 30;
 const xAxis = Math.round(canvasPlotWidth/scaleX/2) * scaleX;
 const yAxis = Math.round(canvasPlotHeight/scaleY/2) * scaleY;
 
-let x1 = Number(document.getElementById('x1').value);
-let x2 = Number(document.getElementById('x2').value);
-let y1 = Number(document.getElementById('y1').value);
-let y2 = Number(document.getElementById('y2').value);
-
-let x3 = Number(document.getElementById('x3').value);
-let x4 = Number(document.getElementById('x4').value);
-let y3 = Number(document.getElementById('y3').value);
-let y4 = Number(document.getElementById('y4').value);
-
-let oneRectWidth = x1 + x2;
-let oneRectHeight = y1 + y2;
-let secRectWidth = x3 + x4;
-let secRectHeight = y3 + y4;
-
 function draw() {
   ctx.beginPath();
   ctx.strokeStyle = '#f5f0e8';
@@ -68,30 +53,36 @@ function draw() {
 }
 
     function check() {
-      let form = document.getElementById('form');
+let x1 = Number(document.getElementById('x1').value)*10;
+let x2 = Number(document.getElementById('x2').value)*10;
+let y1 = Number(document.getElementById('y1').value)*10;
+let y2 = Number(document.getElementById('y2').value)*10;
+
+let x3 = Number(document.getElementById('x3').value)*10;
+let x4 = Number(document.getElementById('x4').value)*10;
+let y3 = Number(document.getElementById('y3').value)*10;
+let y4 = Number(document.getElementById('y4').value)*10;
+
+let oneRectWidth = x1 + x2;
+let oneRectHeight = y1 + y2;
+let secRectWidth = x3 + x4;
+let secRectHeight = y3 + y4;
       if(x3>x2 || x1>x4) { // right & left
         alert('not cross');
       } else if (y3>y2 || y1>y4) { // top & bottom
         alert('not cross');
-      } else if(form.elements === ' '){
-        alert('type some parameters');
       } else {
         alert('cross');
-      }
-    drawRectangles(ctx);
-    }
-
-    function drawRectangles(ctx) {    
-        /*ctx.clearRect(x1,y1, oneRectWidth, oneRectHeight);*/
-        ctx.beginPath();
-        ctx.translate(xAxis,yAxis);
-        ctx.rotate(90 * Math.PI / 180);
+      }  
+      
+        ctx.translate(250, 250);
+        ctx.scale(1,-1);
         ctx.fillStyle = 'red';
         ctx.fillRect(x1,y1,oneRectWidth,oneRectHeight);
         
         ctx.fillStyle = "blue"; 
         ctx.fillRect(x3, y3, secRectWidth, secRectHeight);
-        ctx.closePath();
+
       }
 
       function reset() {
@@ -99,5 +90,7 @@ function draw() {
         ctx.clearRect(x1,y1,oneRectWidth,oneRectHeight);
         ctx.clearRect(x2,y2,secRectWidth,secRectHeight)
       }
+
+      
         
      
